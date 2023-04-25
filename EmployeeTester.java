4a44a811fc2cd4f54a55236ac417c7c6d0d376d1
Omkar Yadav <130984348+omkaryadav4433@users.noreply.github.com>
2023-04-25 19:40:25 +0530

    Add files via upload

---
+++ EmployeeTester.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class EmployeeTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+	Scanner sc=new Scanner(System.in);
+	
+	Employee emp=new Employee();
+		
+		
+		
+		Employee e2= new Employee();
+		System.out.println(e2.toString());
+		
+		
+		
+		 
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
