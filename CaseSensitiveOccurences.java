eba48dd61805ec963145e8e5c4ba2ab08fbefb31
Somesh Dattatraya Dahotre <131247687+totally-somesh@users.noreply.github.com>
2023-04-27 00:10:00 +0530

    Occurrence Of A Character In A String
    ~ Here, two different cases are done as per case sensitive or not.
    ~ In built methods given by JAVA i.e. charAt() / toLowerCase() etc. are used.

---
+++ CaseSensitiveOccurences.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CaseSensitiveOccurences {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter A Sentence : ");
+		String str = sc.nextLine();
+
+		System.out.println("Enter A Character To Search : ");
+		char L = sc.next().charAt(0);
+
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == L) {
+				count++;
+			}
+		}
+		System.out.println("Entered Character : " + L + " Is Present For " + count + " Times.");
+	}
+}
